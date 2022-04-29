2883e4048ab818ef9ae373fbf729e67b027c8743
joaoGuiotti012 <gente.boa@hotmail.com>
2022-04-29 15:08:04 -0300

    ajustes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { map, Observable } from 'rxjs';
 import { IProduct } from 'src/app/models/product.model';
@@ -17,7 +18,7 @@ export class HeaderComponent implements OnInit {
   isAuth$: Observable<boolean>;
   user$!: Observable<any>;
 
-  constructor(private store: Store<any>) {
+  constructor(private store: Store<any>, private router: Router) {
     this.cart$ = store.select('products').pipe(map((state) => state.cart));
     this.isAuth$ = this.store.select(isAuthenticated);
     this.user$ = this.store.select('auth').pipe(map((state) => state.user));
@@ -25,8 +26,12 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  logout() {
+  logout(): void {
     this.store.dispatch(autoLogout())
+  }
+
+  redirectHome(): void {
+    this.router.navigate(['/home']);
   }
 
 }
